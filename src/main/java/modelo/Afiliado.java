0ce6f331a1ec310d059525161f4dd0f3ec3c625e
DanielS0411 <159973433+DanielS0411@users.noreply.github.com>
2024-05-23 16:32:03 -0500

    Se genera actualizacion del pom ya que no tenia descargado los paquetes de jakarta, se actualizan clases, con relaciones con getters y setter.

--- src/main/java/modelo/Afiliado.java
+++ src/main/java/modelo/Afiliado.java
@@ -1,4 +1,43 @@
 package modelo;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Afiliado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private Afiliado afiliado;
+    private String apellidos;
+    private String cedula;
+
+    public Afiliado(Afiliado afiliado, String apellidos, String cedula) {
+        this.afiliado = afiliado;
+        this.apellidos = apellidos;
+        this.cedula = cedula;
+    }
+    public Afiliado getAfiliado() {
+        return afiliado;
+    }
+
+    public void setAfiliado(Afiliado afiliado) {
+        this.afiliado = afiliado;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
 }
